c216b17b03c4ee872cef1986d7a7d865f1c5fcd9
Crowdbotics <engineering@crowdbotics.com>
2022-06-10 01:00:52 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,10 @@
+from django.conf import settings
 from django.db import models
+
+
+class Product(models.Model):
+    "Generated Model"
+    product_name = models.CharField(
+        max_length=10,
+    )
+    price = models.FloatField()
--- backend/max_api_test_dev_24681/wsgi.py
+++ backend/max_api_test_dev_24681/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'max_api_test_dev_24681.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "max_api_test_dev_24681.settings")
 
 application = get_wsgi_application()
